284de42f1fa6fd7234dc55251bf611ed12adeb57
Matthew Dodson <mdodson1023@gmail.com>
2012-12-26 12:33:30 -0500

    Correction to DB creds

--- scrummager/settings.py
+++ scrummager/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'scrummager',                      # Or path to database file if using sqlite3.
-        'USER': 'Agile2438',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'USER': 'scrummager',                      # Not used with sqlite3.
+        'PASSWORD': 'Agile2438',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
