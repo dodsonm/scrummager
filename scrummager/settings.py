180a0ca4be91ba38370a48c6a7c46c7982a3fab7
Matthew Dodson <mdodson1023@gmail.com>
2013-01-03 17:36:29 -0500

    Latest models, tastypie integrated

--- scrummager/settings.py
+++ scrummager/settings.py
@@ -48,7 +48,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/Users/mld/Sites/scrummager.pet.proj/site_assets/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/Users/mld/Sites/scrummager.pet.proj/site_assets/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/mld/Sites/scrummager.pet.proj/scrummager/scrummager/apps/frontend/templates/'
 )
 
 INSTALLED_APPS = (
@@ -119,13 +120,19 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
 
 INSTALLED_APPS += (
+    'scrummager.apps.frontend',
+    'scrummager.apps.standup',
+)
+
+INSTALLED_APPS += (
     'south',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
--- scrummager/urls.py
+++ scrummager/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls import patterns, include, url
 
+
+#tastypie
+from apps.standup.api import ProjectResource, ReportResource
+project_resource = ProjectResource()
+report_resource = ReportResource()
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scrummager.views.home', name='home'),
-    # url(r'^scrummager/', include('scrummager.foo.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^standup/', include('scrummager.apps.standup.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #tastypie
+    (r'^api/', include(project_resource.urls)),
+    (r'^api/', include(report_resource.urls)),
 )
